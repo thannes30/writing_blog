06c1e9f9d230edcff5631a016bdd2f02e44671db
thannes30 <thannes30@yahoo.com>
2014-11-17 20:46:58 -0500

    added ajax commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,10 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(comment_params)
-    redirect_to @post
+    respond_to do |format|
+      format.html {redirect_to @post}
+      format.js
+    end
   end
 
   private
