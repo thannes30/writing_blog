edb1a8fc5c6e2c593a646180a5364f1dc5bd9d58
thannes30 <thannes30@yahoo.com>
2014-11-17 20:58:40 -0500

    added rss feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,12 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+      format.atom
+    end
   end
 
   # GET /posts/1
