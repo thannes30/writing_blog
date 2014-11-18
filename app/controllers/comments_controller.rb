812a3ef72331b81216fd83a63e3c45ee0bf1de83
thannes30 <thannes30@yahoo.com>
2014-11-17 20:25:46 -0500

    pushing to heroku

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,14 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params[:comment])
+    @comment = @post.comments.create!(comment_params)
     redirect_to @post
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :body)
   end
 
 end
