acf52d78c1bb81e97570c6c886c557fb170f34a1
thannes30 <thannes30@yahoo.com>
2014-11-25 19:16:27 -0500

    basic testing completed for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'should validate presence of title' do
+    post = Post.new
+    post.valid?
+    expect(post.errors.messages[:title]).to include "can't be blank"
+
+  #   [:title, :body].each do |attribute|
+  #     it "should validate presence of #{attribute}" do
+  #       expect(post).to have_at_least(1).error_on(attribute)
+  #       expect(post.errors.messages[attribute]).to include "can't be blank"
+  #     end
+  #   end
+  end
 end
